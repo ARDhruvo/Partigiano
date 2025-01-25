141b88cbf5249b8d30dd6c92eefffe1fb478dad8
Yahya <takeyahya6@gmail.com>
2025-01-25 18:55:22 +0600

    profile and aboutus added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Home from "./pages/home";
 import Category from "./pages/category";
+import Profile from "./pages/profile";
+import AboutUs from "./pages/aboutUs";
 import Login from "./pages/Login";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
@@ -17,6 +19,8 @@ function App() {
         <Route path="category" element={<Category/>}/>
         <Route path="category/:id" element={<Category/>}/>
         <Route path="login" element={<Login/>}/>
+        <Route path="profile" element={<Profile/>}/>
+        <Route path="aboutUs" element={<AboutUs/>}/>
       </Routes>
     </>
   );
