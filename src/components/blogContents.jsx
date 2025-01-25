225ea41ec5acec540a6513ec88213252d8c1ff0a
ARDhruvo <aribur.cse.20220204003@aust.edu>
2025-01-26 00:03:45 +0600

    CSS Semi Final Update

--- src/components/blogContents.jsx
+++ src/components/blogContents.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function blogContents(props) {
-    const blogs=props.blogs;
+  const blogs = props.blogs;
   return (
-    <div className='blog-contents'>
-        {blogs.map((blog) => (
-            <div className='blog-preview' key={blog.id}>
-                <h2>{ blog.title }</h2>
-                <p>Written by {blog.author}</p>
-            </div>
-        ))}
+    <div className="blog-contents">
+      {blogs.map((blog) => (
+        <div className="category-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default blogContents;
--- src/components/categoryBody.jsx
+++ src/components/categoryBody.jsx
@@ -1,26 +1,44 @@
-import React from 'react'
-import { useState } from 'react';
-import BlogContents from './blogContents';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useState } from "react";
+import BlogContents from "./blogContents";
+import { useParams } from "react-router-dom";
 
 function categoryBody() {
+  const { id } = useParams();
 
-  const { id } =useParams();
+  const [blogs, setBlogs] = useState([
+    {
+      category: "Education",
+      title: "Hello Education",
+      body: "Lorem ipsen......",
+      author: "Nazim",
+      id: 1,
+    },
+    {
+      category: "Emergency",
+      title: "Hello Emergency",
+      body: "Lorem ipsen......",
+      author: "Raian",
+      id: 2,
+    },
+    {
+      category: "Education2",
+      title: "Hello Education1",
+      body: "Lorem ipsen......",
+      author: "Khan",
+      id: 3,
+    },
+  ]);
 
-  const [blogs,setBlogs] = useState([
-      { category:'Education', title: 'Hello Education', body: 'Lorem ipsen......', author:'Nazim' , id: 1},
-      { category:'Emergency', title: 'Hello Emergency', body: 'Lorem ipsen......', author:'Raian' , id: 2},
-      { category:'Education2', title: 'Hello Education1', body: 'Lorem ipsen......', author:'Khan' , id: 3},
-    ]); 
-    
   return (
-    <div className='category-content'>
-        <div className='blog-category'>
-          <h2>Category : {id}</h2>
-        </div>
-        <BlogContents blogs={blogs.filter((blog) => blog.category === id  )}/>
+    <div className="home-content">
+      <div className="homeTitle">
+        Category : {id}
+        <hr />
+      </div>
+      <BlogContents blogs={blogs.filter((blog) => blog.category === id)} />
     </div>
-  )
+  );
 }
 
-export default categoryBody
+export default categoryBody;
