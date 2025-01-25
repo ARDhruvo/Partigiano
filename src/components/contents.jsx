30392e2dfbc9c6dbb1d430f75b45b7866bde87a9
Nazim Raian Khan <nazimraiank@gmail.com>
2025-01-25 23:58:18 +0600

    author name added

--- src/components/contents.jsx
+++ src/components/contents.jsx
@@ -6,7 +6,7 @@ function contents(props) {
       <div className='content'>
           {contents.map((content) => (
               <div className='content-preview' key={content.id}>
-                
+                  <h4>{content.author}</h4>  
                   <h2>{ content.title }</h2>
                   <p> {content.body} </p>
                 
