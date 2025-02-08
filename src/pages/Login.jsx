01b01b4626d311a24cfa35efc1d343b7db288604
ARDhruvo <aribur.cse.20220204003@aust.edu>
2025-02-08 14:30:05 +0600

    Login Page with Formik and Yup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import TextField from "@mui/material/TextField";
-import Field from "../components/Field";
-import { Link } from "react-router-dom";
-import { Formik, Form, /*Field,*/ ErrorMessage } from "formik";
+import { Link, useNavigate } from "react-router-dom";
+import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 import "./Login.css";
 
 function Login() {
   const [isLogin, setIsLogin] = useState(true);
+  const navigate = useNavigate();
 
   const loginSchema = Yup.object().shape({
     username: Yup.string().required("Username is required"),
@@ -25,9 +25,22 @@ function Login() {
       .required("Confirm password is required"),
   });
 
-  const handleSubmit = (values, { setSubmitting }) => {
-    // Handle form submission here
-    console.log(values);
+  const handleSubmit = (values, { setSubmitting, setErrors }) => {
+    if (isLogin) {
+      if (values.username && values.password) {
+        // Handle successful login here
+        // console.log(values);
+        navigate("/");
+      } else {
+        setErrors({
+          username: !values.username ? "Username is required" : undefined,
+          password: !values.password ? "Password is required" : undefined,
+        });
+      }
+    } else {
+      // Handle signup logic here
+      console.log(values);
+    }
     setSubmitting(false);
   };
 
@@ -53,74 +66,97 @@ function Login() {
         }}
         validationSchema={isLogin ? loginSchema : signupSchema}
         onSubmit={handleSubmit}
+        validateOnChange={true}
+        validateOnBlur={true}
       >
-        {({ isSubmitting }) => (
+        {({ isSubmitting, errors, touched, values }) => (
           <Form>
             {!isLogin && (
               <div className="textbox">
-                <Field
-                  title="E-Mail"
-                  type="email"
-                  name="email"
-                  placeholder="E-Mail"
-                />
-                <ErrorMessage name="email" component="div" className="error" />
+                <Field name="email">
+                  {({ field, meta }) => (
+                    <TextField
+                      {...field}
+                      label="E-Mail"
+                      type="email"
+                      placeholder="E-Mail"
+                      fullWidth
+                      margin="normal"
+                      error={meta.touched && !!meta.error}
+                      helperText={meta.touched && meta.error}
+                    />
+                  )}
+                </Field>
               </div>
             )}
             <div className="textbox">
-              <Field
-                title="Username"
-                type="text"
-                name="username"
-                placeholder="Username"
-              />
-              <ErrorMessage name="username" component="div" className="error" />
+              <Field name="username">
+                {({ field, meta }) => (
+                  <TextField
+                    {...field}
+                    label="Username"
+                    type="text"
+                    placeholder="Username"
+                    fullWidth
+                    margin="normal"
+                    error={touched.username && !!errors.username}
+                    helperText={touched.username && errors.username}
+                  />
+                )}
+              </Field>
             </div>
             <div className="textbox">
-              <Field
-                title="Password"
-                vis="password"
-                type="password"
-                name="password"
-                placeholder="Password"
-              />
-              <ErrorMessage name="password" component="div" className="error" />
+              <Field name="password">
+                {({ field, meta }) => (
+                  <TextField
+                    {...field}
+                    label="Password"
+                    type="password"
+                    placeholder="Password"
+                    fullWidth
+                    margin="normal"
+                    error={touched.password && !!errors.password}
+                    helperText={touched.password && errors.password}
+                  />
+                )}
+              </Field>
             </div>
             {!isLogin && (
               <div className="textbox">
-                <Field
-                  title="Confirm Password"
-                  vis="password"
-                  type="password"
-                  name="confirmPassword"
-                  placeholder="Confirm Password"
-                />
-                <ErrorMessage
-                  name="confirmPassword"
-                  component="div"
-                  className="error"
-                />
+                <Field name="confirmPassword">
+                  {({ field, meta }) => (
+                    <TextField
+                      {...field}
+                      label="Confirm Password"
+                      type="password"
+                      placeholder="Confirm Password"
+                      fullWidth
+                      margin="normal"
+                      error={meta.touched && !!meta.error}
+                      helperText={meta.touched && meta.error}
+                    />
+                  )}
+                </Field>
               </div>
             )}
             <div>
-              <Link to="/">
-                <button
-                  type="submit"
-                  className={isLogin ? "btnLog" : "btnSign"}
-                  /*disabled={isSubmitting}*/
-                >
+              {isLogin && values.username && values.password ? (
+                <Link to="/">
+                  <button type="submit" className="btn">
+                    Log In
+                  </button>
+                </Link>
+              ) : (
+                <button type="submit" className="btn">
                   {isLogin ? "Log In" : "Sign Up"}
                 </button>
-              </Link>
+              )}
             </div>
           </Form>
         )}
       </Formik>
       <div>
-        <button
-          className={isLogin ? "inactbtnSign" : "inactbtnLog"}
-          onClick={toggleMode}
-        >
+        <button className="inactBtn" onClick={toggleMode}>
           {isLogin ? "Sign Up Instead?" : "Log In Instead?"}
         </button>
       </div>
