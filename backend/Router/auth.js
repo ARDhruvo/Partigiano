ae0243d44894337dd72019f5022e1357a9d23c2d
ARDhruvo <aribur.cse.20220204003@aust.edu>
2025-03-07 20:13:21 +0600

    Logout in main

--- backend/Router/auth.js
+++ backend/Router/auth.js
@@ -2,15 +2,18 @@ import express from "express";
 import {
   signupUser,
   loginUser,
+  logoutUser,
   verifyOTP,
 } from "../Controller/userController.js";
 import { validateSignup, validateLogin } from "../Middleware/userMiddleware.js";
 import { validateOTP } from "../Middleware/otpMiddleware.js";
+import checkToken from "../Middleware/checkToken.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/signup", validateSignup, signupUser);
 userRouter.post("/login", validateLogin, loginUser);
+userRouter.post("/logout", checkToken, logoutUser);
 userRouter.post("/verifyotp", validateOTP, verifyOTP);
 
 export default userRouter;
