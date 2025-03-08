a3b670a061a393808bccb59a75887e8ae1d2c3e3
Nazim Raian Khan <nazimraiank@gmail.com>
2025-03-09 00:01:12 +0600

    Reported Post in admin Page

--- src/components/adminBody.jsx
+++ src/components/adminBody.jsx
@@ -1,14 +1,49 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import BlogContents from "./blogContents";
+import { useParams } from "react-router-dom";
+import "./home.css";
 
 function adminBody() {
+  const { category } = useParams();
+
+  const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchCategoryBlogs = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(`http://localhost:4000/posts`); //${category}
+        const reportedBlogs = response.data
+        .filter((blog) => blog.reports > 0) // Keep only reported blogs
+        .sort((a, b) => b.reports - a.reports);
+        setBlogs(reportedBlogs);
+      } catch (error) {
+        console.error("Error fetching category blogs:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCategoryBlogs();
+  }, [category]);
+
+
   return (
     <div className="home-content">
       <div className="homeTitle">
-        Reported posts :
+        Reported Posts:
         <hr />
       </div>
+      
+      <BlogContents
+        blogs={blogs}
+      />
+      
     </div>
-  )
+  );
 }
 
 export default adminBody;
